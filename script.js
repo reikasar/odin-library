1153cc883f3cc353520091886ecb567de9d5bf03
reikasar <183247743+reikasar@users.noreply.github.com>
2026-03-19 21:33:06 +0200

    add class instead of constructor for book obj

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-};
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+}
 
 function render() {
   const libraryContainer = document.querySelector("#library");
